6d57522c8f09c04e6a03ccf5219c5d199af1978a
theGeekkit <sonofgondolin16@gmail.com>
2024-09-16 20:34:38 -0500

    beginning 2D Adventure

---
+++ My2DGame/src/main/GamePanel.java
@@ -0,0 +1,106 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+	
+	// SCREEN SETTINGS
+	final int originalTileSize = 16; // 16x16 size
+	final int scale = 3;
+	
+	final int tileSize = originalTileSize * scale;
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+	
+	KeyHandler keyH = new KeyHandler();
+	Thread gameThread; 
+	
+	
+	// Set player's default position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
+	
+	
+	public GamePanel () {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+	}
+	
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	@Override
+	public void run() {
+		
+		
+		while(gameThread != null) {
+			
+			
+			long currentTime = System.nanoTime();
+			
+			// 1 UPDATE: update information such as character positions
+			update();
+			
+			// 2 DRAW: draw the screen with the updated information
+			repaint(); 
+		}
+		
+	}
+	public void update() {
+		
+		if(keyH.upPressed == true) {
+			playerY -= playerSpeed;
+		}
+		else if(keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		else if(keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		else if(keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.white);
+		
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
+		
+		g2.dispose();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
